c1db894183922ba0756b3c7b88462b618b66bee1
Dylan Ruff <dylan.ruff@mail.utoronto.ca>
2022-01-25 16:09:02 -0500

    adding marker to map

--- Mapping_Single_Points/static/js/logic.js
+++ Mapping_Single_Points/static/js/logic.js
@@ -16,7 +16,5 @@ let streets = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{
 // Then we add our 'graymap' tile layer to the map.
 streets.addTo(map);
 
-var marker = L.marker([51.5, -0.09]).addTo(map);
-
 //  Add a marker to the map for Los Angeles, California.
 let marker = L.marker([34.0522, -118.2437]).addTo(map);
