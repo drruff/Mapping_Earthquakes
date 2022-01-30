100759a0f61e0cc4ba9e1a9c21a82094f57e0734
Dylan Ruff <dylan.ruff@mail.utoronto.ca>
2022-01-29 23:59:05 -0500

    added earthquake data as a toggable layer

--- Earthquakes_past7days/static/js/logicStep4.js
+++ Earthquakes_past7days/static/js/logicStep4.js
@@ -24,6 +24,15 @@ let baseMaps = {
   "Satellite": satelliteStreets
 };
 
+// Create the earthquake layer for our map.
+let earthquakes = new L.layerGroup();
+
+// We define an object that contains the overlays.
+// This overlay will be visible all the time.
+let overlays = {
+    Earthquakes: earthquakes
+  };
+
 // Create the map object with center, zoom level and default layer.
 let map = L.map('mapid', {
   center: [39.5, -98.5],
@@ -31,8 +40,9 @@ let map = L.map('mapid', {
   layers: [streets]
 });
 
-// Pass our map layers into our layers control and add the layers control to the map.
-L.control.layers(baseMaps).addTo(map);
+// Then we add a control to the map that will allow the user to change
+// which layers are visible.
+L.control.layers(baseMaps, overlays).addTo(map);
 
 // Accessing last 7 days earthquakes url
 let Earthquakes7days = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
@@ -98,6 +108,8 @@ d3.json(Earthquakes7days).then(function(data) {
     onEachFeature: function(feature, layer) {
     layer.bindPopup("Magnitude: " + feature.properties.mag + "<br>Location: " + feature.properties.place);
   }
-    }).addTo(map);
-});
+    }).addTo(earthquakes);
 
+    // Then we add our earthquakes layer to the map
+    earthquakes.addTo(map);
+});
